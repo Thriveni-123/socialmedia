5e1600dec44b4a55006c3c82bd248dd2c96e67e5
Thriveni-123 <71956386+Thriveni-123@users.noreply.github.com>
2020-09-30 16:13:51 +0530

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const joiSchemaValidation = require('../middleware/joiSchemaValidation');
+const userController = require('../controller/userController');
+const userSchema = require('../apiSchema/userSchema');
+
+module.exports = router;
+
+router.post('/login', joiSchemaValidation.validateBody(userSchema.login),
+userController.Login
+);
+
+router.post('/add',
+userController.Addpost
+);
+
+router.post('/update',
+userController.Updatepost
+);
+router.post('/delete',
+userController.Delete
+);
