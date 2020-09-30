6eeef119861dd5b63c3b5305b415df45af5746b5
Thriveni-123 <71956386+Thriveni-123@users.noreply.github.com>
2020-09-30 17:58:49 +0530

    Add files via upload

--- constants/index.js
+++ constants/index.js
@@ -8,21 +8,10 @@ module.exports = {
         BAD_REQUEST: 'Invalid fields'
     },
     USERMESSAGE:{
-        SIGNUP:'Registration completed successfully',
-        ALREADY_REGISTER:'Email is already registered',
-        REGISTER:'You have already registered with RTA . Please try logging in. ',
+        LOGIN:'Login successfully',
         INCORRECT_PASSWORD:'Incorrect Password. Please try again',
-        SIGNIN:'Signin successfully',
-       
-    },
-    PROFILEMESSAGE:{
-        ADD:'Adding your details successfully',
-        UPDATE:'Update your profile successfully'
-    },
-    COURSEMESSAGE:{
-    CREATE:'Create the course detailssuccessfully',
-    NOTALLOW:'Your are not allow to create course',
-    ALLCOURSE:'Fetch all course successfully',
-    STUDENTCREATE:'Fetch student created course'
+        ADD:'Adding your post successfully',
+        UPDATE:'Update your post successfully',
+        DELETE:'Delete your post successfully'  
     }
 }
