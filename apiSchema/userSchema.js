46372ec819d71c9f14328b9ed5b254b6e1936c12
Thriveni-123 <71956386+Thriveni-123@users.noreply.github.com>
2020-09-30 17:55:14 +0530

    Add files via upload

--- apiSchema/userSchema.js
+++ apiSchema/userSchema.js
@@ -4,3 +4,7 @@ module.exports.login= Joi.object().keys({
     email:Joi.string().email().required(),
     password:Joi.string().required()
 });
+
+module.exports.delete= Joi.object().keys({
+    email:Joi.string().email().required(),
+});
